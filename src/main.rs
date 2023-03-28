79b6c2fc46c91aa62e18716cf6de15c87f220fdd
Hukasx0 <hubertkasp13@gmail.com>
2023-03-29 00:32:03 +0200

    Added more operations on posts API

--- src/main.rs
+++ src/main.rs
@@ -9,34 +9,34 @@ static YOUR_DESCRIPTION: &str = "test description, which is a placeholder";
 
 #[get("/")]
 async fn index() -> HttpResponse {
-    let indexFile = fs::read_to_string("web/index.html")
+    let index_file = fs::read_to_string("web/index.html")
     .expect("Problem with reading index.html file");
-    HttpResponse::Ok().body(indexFile.replace("{{author_name}}",YOUR_NAME).replace("{{author_description}}",YOUR_DESCRIPTION))
+    HttpResponse::Ok().body(index_file.replace("{{author_name}}",YOUR_NAME).replace("{{author_description}}",YOUR_DESCRIPTION))
 }
 
 #[get("/posts")]
 async fn list_posts(posts: web::Data<Posts>) -> HttpResponse {
-    let postsFile = fs::read_to_string("web/posts.html")
+    let posts_file = fs::read_to_string("web/posts.html")
     .expect("Problem with reading posts.html file");
     let mut post_list = String::new();
-    for post in posts.get_list().iter() {
+    for post in posts.get_list().iter().rev() {
     	post_list.push_str(&format!(r#"
 	<div class="post">
-	   <h2 class="title">{}</h2>
+	   <a href="/post/{}"<h2 class="title">{}</h2></a>
 	   <p class="description">{}</p>
 	   <span class="date">{}</span>
 	</div>
-	"#, post.name, post.text, post.date));
+	"#,post.id ,post.name, post.text, post.date));
     }
-    HttpResponse::Ok().body(postsFile.replace("{{author_name}}",YOUR_NAME).replace("{{post_list}}",&post_list))
+    HttpResponse::Ok().body(posts_file.replace("{{author_name}}",YOUR_NAME).replace("{{post_list}}",&post_list))
 }
 
 #[get("/post/{pid}")]
 async fn get_post(pid: actix_web::web::Path<usize>, posts: web::Data<Posts>) -> HttpResponse {
-    let postFile = fs::read_to_string("web/post.html")
+    let post_file = fs::read_to_string("web/post.html")
     .expect("Problem with reading post.html file");
     if let Some(post) = posts.get_post(pid.into_inner()) {
-       HttpResponse::Ok().body(postFile.replace("{{post_name}}",&post.name).replace("{{post_text}}",&post.text).replace("{{post_date}}",&post.date).replace("{{author_name}}",YOUR_NAME))
+       HttpResponse::Ok().body(post_file.replace("{{post_name}}",&post.name).replace("{{post_text}}",&post.text).replace("{{post_date}}",&post.date).replace("{{author_name}}",YOUR_NAME))
     } else {
       HttpResponse::Ok().body("Post with this id does not exist")
     }
@@ -60,21 +60,41 @@ async fn add_post(form: web::Form<AddPost>, posts: web::Data<Posts>) -> HttpResp
    HttpResponse::Ok().body(format!("Added {} to database",&form.name))
 }
 
-#[post("/api/modifyPost")]
-async fn modify_post() -> HttpResponse {
-    HttpResponse::Ok().body("Hello world!")
+#[derive(Deserialize)]
+struct ModPost {
+   id: usize,
+   name: String,
+   text: String,
+   date: String,
+}
+
+#[post("/api/editPost")]
+async fn modify_post(form: web::Form<ModPost>, posts: web::Data<Posts>) -> HttpResponse {
+    posts.edit_post(Post {
+       id: form.id,
+       name: form.name.to_string(),
+       text: form.text.to_string(),
+       date: form.date.to_string(),
+    });
+    HttpResponse::Ok().body(format!("Modified {} post in database",form.id))
+}
+
+#[derive(Deserialize)]
+struct RmPost {
+   id: usize,
 }
 
 #[post("/api/removePost")]
-async fn remove_post() -> HttpResponse {
-    HttpResponse::Ok().body("Hello world!")
+async fn remove_post(form: web::Form<RmPost>, posts: web::Data<Posts>) -> HttpResponse {
+    posts.rm_post(form.id);
+    HttpResponse::Ok().body(format!("Post with id {} has been removed",form.id))
 }
 
 #[get("/css/main.css")]
 async fn css_main() -> HttpResponse {
-    let cssFile = fs::read_to_string("web/css/main.css")
+    let css_file = fs::read_to_string("web/css/main.css")
     .expect("Problem with reading main.css file");
-    HttpResponse::Ok().body(cssFile)
+    HttpResponse::Ok().body(css_file)
 }
 
 #[actix_web::main]
--- src/posts.rs
+++ src/posts.rs
@@ -1,5 +1,4 @@
 use std::sync::{Arc, Mutex};
-use std::ops::Deref;
 
 #[derive(Clone)]
 pub struct Post {
@@ -25,11 +24,33 @@ impl Posts {
 
    pub fn get_post(&self, id: usize) -> Option<Post> {
       let posts = self.posts.lock().unwrap();
-      posts.get(id).cloned()
+      posts.iter().find(|post| post.id == id).cloned()
    }
 
    pub fn push_post(&self, post: Post) {
        let mut posts = self.posts.lock().unwrap();
-       posts.push(post);
+       let posts_len = posts.len();
+       let mut last_post_id: usize = 0;
+       if posts_len > 0 {
+          last_post_id = &posts[posts_len-1].id+1;
+       }
+       posts.push(Post {
+          id: last_post_id,
+	  name: post.name,
+	  text: post.text,
+	  date: post.date,
+       });
+   }
+
+   pub fn edit_post(&self, post: Post) {
+       let mut posts = self.posts.lock().unwrap();
+       if let Some(post_to_update) = posts.iter_mut().find(|cpost| cpost.id == post.id) {
+          *post_to_update = post;
+       }
+   }
+
+   pub fn rm_post(&self, id: usize) {
+       let mut posts = self.posts.lock().unwrap();
+       posts.retain(|post| post.id != id);
    }
 }
